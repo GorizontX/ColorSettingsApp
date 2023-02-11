3fab6a3932bbb4db37ce745c1cd7970187990d5d
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-02-11 22:13:56 +0100

    1. Connected the interface elements with the code 2. Set up a private method to configure the slider

--- ColorSettingsApp/ViewController.swift
+++ ColorSettingsApp/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var numberOfRedLabel: UILabel!
+    @IBOutlet var numberOfGreenLabel: UILabel!
+    @IBOutlet var numberOfBlueLabel: UILabel!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        colorView.layer.cornerRadius = 10
+        setUpSlider()
+        
     }
-
-
+    
+    // MARK: - IB Action
+    
+    @IBAction func redSliderAction() {
+       
+        
+    }
+    @IBAction func greenSliderAction() {
+        numberOfGreenLabel.text = "\(greenSlider.value)"
+    }
+    
+    @IBAction func blueSliderAction() {
+        numberOfBlueLabel.text = "\(blueSlider.value)"
+    }
+    
+    
+    // MARK: - Privat Methods
+    private func setUpSlider() {
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
 }
+
 
