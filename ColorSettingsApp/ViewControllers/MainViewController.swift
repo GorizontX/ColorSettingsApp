2d395936fac2df631739c3c7397615c82197c00a
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-04-06 18:44:57 +0200

    Finished the code delegating information from one screen to another.

---
+++ ColorSettingsApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  ColorSettingsApp
+//
+//  Created by Andrey Machulin on 04.04.2023.
+//
+
+import UIKit
+
+protocol SettingsViewControllersDelegte {
+    func setColor(_ color: UIColor)
+}
+
+class MainViewController: UIViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.setSolor = view.backgroundColor
+    }
+    
+}
+
+// MARK: - ColorDelegate
+extension MainViewController: SettingsViewControllersDelegte {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+    
+    
+}
---
+++ ColorSettingsApp/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  ColorSettingsApp
+//
+//  Created by Andrey Machulin on 10.02.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    var delegate: SettingsViewControllersDelegte!
+    var setSolor: UIColor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorView.layer.cornerRadius = 15
+        
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - IB Action
+    
+    @IBAction func rgbSliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    @IBAction func saveButtonPressed() {
+        delegate.setColor(colorView.backgroundColor ?? .black)
+        dismiss(animated: true)
+    }
+    
+    // MARK: - Privat Methods
+    private func setColor() {
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+}
+
+
